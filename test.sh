09aa52cd1ef90261330e4b2f7ddfb624e71f05ff
trieutrinh <trieutrinh2@gmail.com>
2024-05-12 20:32:42 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -135,6 +135,7 @@ EOF
 }
 
 config_firewall() {
+    yum install -y firewalld || echo "Firewalld installed"
     firewall-cmd --permanent --add-port=10050/tcp 
     firewall-cmd --permanent --add-port=10051/tcp 
     firewall-cmd --permanent --add-port=80/tcp
@@ -160,8 +161,8 @@ result() {
 install_on_rhel
 install_db 
 config_zabbix
-config_firewall
 reload_software
+config_firewall
 result
 
 # Config default
